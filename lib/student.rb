b5633e40cdfb91df66569378ccbb2e01942492e3
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 06:04:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,6 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize (name, grade, id = nil)
-    @name, @grade, @id = name, grade, id
+    @name, @grade = name, grade
   end
 end
