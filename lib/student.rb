03656e614f33fdd8ab32553d40b27488a2238cbc
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 06:06:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,14 @@ class Student
   def initialize (name, grade, id = nil)
     @name, @grade = name, grade
   end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    );
+    SQL
+  end
 end
